75fa29effe486a2a29c55cb0c34982181a5fcc96
Petr Pechacek <petr.pechacek@gmail.com>
2012-04-28 14:23:52 +0200

    Implement for testing

--- client/CollisionObject.dart
+++ client/CollisionObject.dart
@@ -1,36 +1,59 @@
 class CollisionObject {
   
-  double x, y;
   int width, height;
-  double direction;
-  double speed; 
+  double x, y, direction, speed; // TODO: speed zatim px/tick, lepe spocitat z kontanty 
   bool visible;
   
-  void onCollision() {
+  CollisionObject(this.x, this.y, this.width, this.height, this.direction, this.speed) {
     
   }
   
-  void changePosition() {  // vymyslet mozne prejmenovani
-    this.x = this.x + speed / Math.sin(direction);
-    this.y = this.y + speed * Math.sin(direction);
+  void onCollisionWidth(CollisionObject o) {
   }
+  
+  void changePosition() {  // vymyslet mozne prejmenovani
+    if (this.speed != 0.0 && this.direction != 0.0) {
+      this.x += speed * Math.cos(direction);
+      this.y += speed * Math.sin(direction);
+    }
+  }
+  
+  
   
   bool isCollisionWith(CollisionObject o) { // kulicka + mozne dalsi objekty
     
     // top
     if (o.y+o.height > this.y && o.x+o.width > this.x && o.x < this.x + this.width) {
-      this.onCollision();
       return true;
     } 
 
     // bottom
     if (o.y < this.y + this.height && o.x+o.width > this.x && o.x < this.x + this.width) {
-      this.onCollision();
       return true;
     } 
     
-  
+    // left
+    if (o.x + o.width > this.x && o.y+o.height > this.y && o.y < this.y+this.height) {
+      return true;      
+    } 
+
+    // right
+    if (o.x < this.x + this.width && o.y+o.height > this.y && o.y < this.y+this.height) {
+      return true;      
+    } 
+    
     return false;
+    
+    /*
+    
+    return 
+      (o.y+o.height > this.y && o.x+o.width > this.x && o.x < this.x + this.width) ||
+      (o.y < this.y + this.height && o.x+o.width > this.x && o.x < this.x + this.width) ||
+      (o.x + o.width > this.x && o.y+o.height > this.y && o.y < this.y+this.height) ||
+      (o.x < this.x + this.width && o.y+o.height > this.y && o.y < this.y+this.height);
+    
+    */
+    
   }
   
 }
