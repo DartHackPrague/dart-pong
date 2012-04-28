c542a707380ddcdb6b5e93f809317b35250d1674
stepiiik <stepan.zikmund@gmail.com>
2012-04-28 14:41:11 +0200

    Very very VERY alpha version of render method

--- client/Renderer.dart
+++ client/Renderer.dart
@@ -4,6 +4,8 @@ class Renderer {
   Renderer(Arena this.arena);
   
   render() {
-    
+    var ball = document.query('#ball');
+    ball.style.left = arena.ball.x;
+    ball.style.top = arena.ball.y;
   }
 }
