778d0aca2eaf848205355298555709e2bf2b9b1f
Petr Pechacek <petr.pechacek@gmail.com>
2012-04-28 15:28:39 +0200

    Opravene hodnoty u definice hranicnich objeku

--- client/DartPong.dart
+++ client/DartPong.dart
@@ -19,12 +19,12 @@ class DartPong {
 
   void run() {
     List<CollisionObject> map = new List<CollisionObject>();
-    map.add(new CollisionObject( 'leva_stena', 0, 0, 10, 600, 0, 0 ));
-    map.add(new CollisionObject( 'horni_stena', 0, 0, 800, 10, 0, 0 ));
-    map.add(new CollisionObject( 'prava_stena', 790, 0, 10, 600, 0, 0 ));
-    map.add(new CollisionObject( 'spodni_stena', 0, 790, 800, 10, 0, 0 ));
+    map.add(new CollisionObject( 'leva_stena', 0, 0, 1, 600, 0, 0 ));
+    map.add(new CollisionObject( 'horni_stena', 0, 0, 800, 1, 0, 0 ));
+    map.add(new CollisionObject( 'prava_stena', 800, 0, 1, 600, 0, 0 ));
+    map.add(new CollisionObject( 'spodni_stena', 0, 600, 800, 1, 0, 0 ));
     
-    Ball ball = new Ball( 'ball', 300, 300, 10, 10, 0, 1);
+    Ball ball = new Ball( 'ball', 300, 300, 10, 10, 6, 1);
     Arena arena = new Arena( map, ball );
     Renderer renderer = new Renderer( arena );
     Ticker ticker = new Ticker( 10, arena, renderer, window ); 
