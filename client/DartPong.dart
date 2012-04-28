58c9f44588edc557ce66db7c12193415753c6a85
Petr Kohut <petr.kohut@socialbakers.com>
2012-04-28 14:16:14 +0200

    editace hlavni tridy kde dochazi k inicializaci vsech ostatnich trid

--- client/DartPong.dart
+++ client/DartPong.dart
@@ -1,20 +1,28 @@
 #import('dart:html');
 
+#source('Renderer.dart');
+#source('Ticker.dart');
+#source('CollisionObject.dart');
+#source('Arena.dart');
+#source('Ball.dart');
+
+
+void main() {
+  new DartPong();
+}
+
 class DartPong {
 
   DartPong() {
+    this.run();
   }
 
   void run() {
-   
+    List<CollisionObject> map;
+    Ball ball = new Ball();
+    Arena arena = new Arena( map, ball );
+    Renderer renderer = new Renderer( arena );
+    Ticker ticker = new Ticker( 100 );    
   }
 
-  void write(String message) {
-    // the HTML library defines a global "document" variable
-    document.query('#status').innerHTML = message;
-  }
-}
-
-void main() {
-  new DartPong().run();
 }
