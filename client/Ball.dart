1efafc7ba006e7ad0039f7e24f84cfe30a603fef
stepiiik <stepan.zikmund@gmail.com>
2012-04-28 14:38:54 +0200

    Added contructor

--- client/Ball.dart
+++ client/Ball.dart
@@ -1,4 +1,6 @@
 class Ball extends CollisionObject {
+  Ball(x, y, width, height, direction, speed) : super(x, y, width, height, direction, speed);
+  
   void onCollisionWith(CollisionObject o) {
   }
 }
