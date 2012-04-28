3ea4c0f723240628116e48d6f9d267ee58e9f3a6
Petr Pechacek <petr.pechacek@gmail.com>
2012-04-28 13:47:20 +0200

    CollisionObject without errors

--- client/CollisionObject.dart
+++ client/CollisionObject.dart
@@ -1,16 +1,36 @@
 class CollisionObject {
   
-  int x, y;
+  double x, y;
   int width, height;
   double direction;
   double speed; 
-  boolean visible;
+  bool visible;
   
-  onCollision()
+  void onCollision() {
+    
+  }
   
-  changePosition()  / vymyslet mozne prejmenovani
+  void changePosition() {  // vymyslet mozne prejmenovani
+    this.x = this.x + speed / Math.sin(direction);
+    this.y = this.y + speed * Math.sin(direction);
+  }
   
-  isCollisionWith(CollisionObject objectX) // kulicka + mozne dalsi objekty
+  bool isCollisionWith(CollisionObject o) { // kulicka + mozne dalsi objekty
+    
+    // top
+    if (o.y+o.height > this.y && o.x+o.width > this.x && o.x < this.x + this.width) {
+      this.onCollision();
+      return true;
+    } 
+
+    // bottom
+    if (o.y < this.y + this.height && o.x+o.width > this.x && o.x < this.x + this.width) {
+      this.onCollision();
+      return true;
+    } 
+    
   
+    return false;
+  }
   
 }
