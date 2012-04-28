0eac5cc000e04770b131724afaf4a68158a96931
Petr Kohut <petr.kohut@socialbakers.com>
2012-04-28 16:15:03 +0200

    pridani Handleru

--- client/DartPong.dart
+++ client/DartPong.dart
@@ -26,10 +26,12 @@ class DartPong {
     map.add(new CollisionObject( 'prava_stena', 800, 0, 1, 600, 0, 0 ));
     map.add(new CollisionObject( 'spodni_stena', 0, 600, 800, 1, 0, 0 ));
     
-    Ball ball = new Ball( 'ball', 300, 300, 10, 10, 6, 1);
+    Ball ball = new Ball( 'ball', 300, 300, 10, 10, 6, 1 );
+    Handler handler = new Handler( 'handler', 10, 300, 10, 100, 0, 0 );
+    map.add(handler);
     Arena arena = new Arena( map, ball );
     Renderer renderer = new Renderer( arena );
-    Ticker ticker = new Ticker( 10, arena, renderer, window );
+    Ticker ticker = new Ticker( 10, arena, renderer, window );    
     
     ticker.start();
     
--- client/Handler.dart
+++ client/Handler.dart
@@ -1,3 +1,13 @@
-class Handler extends CollisionObject {
+class Handler extends CollisionObject {  
+  
   Handler(id, x, y, width, height, direction, speed) : super(id, x, y, width, height, direction, speed);
+  
+  moveUp() {
+    this.y = this.y - 5;
+  }
+  
+  moveDown() {
+    this.y = this.y + 5;
+  }
+  
 }
