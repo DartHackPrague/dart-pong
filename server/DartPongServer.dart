a7577c34c0aba7e2903b02e4d02ce89efa8f86cf
Petr Kohut <petr.kohut@socialbakers.com>
2012-04-29 15:05:45 +0200

    uprava websocket serveru

--- server/DartPongServer.dart
+++ server/DartPongServer.dart
@@ -3,24 +3,24 @@
 
 void main() {
   
-  var clients = new List();
+  int clients;
   
   HttpServer server = new HttpServer();
   WebSocketHandler wsHandler = new WebSocketHandler();
   server.addRequestHandler((req) => req.path == '/ws', wsHandler.onRequest);
   
   wsHandler.onOpen = (WebSocketConnection conn) {
-    print('new connection, number of connections: ' + clients.length);
+    print('new connection');
     
     conn.onMessage = (message) {
       var parsedMsg = JSON.parse(message);
       var result = {};
       if (parsedMsg['type'] == 1) {
-        print(parsedMsg['name'] + ' has joined to the game.');
-        clients.add(parsedMsg['name']);
-        
+        clients++;
+        print('First player has joined to the game.');
         result['type'] = 1;
-        result['numberOfClients'] = clients.length;
+        result['numberOfClients'] = clients;
+        result['message'] = 'One player is waiting for opponent.';
         conn.send(JSON.stringify(result));
       }
       if (parsedMsg['type'] == 2) {
@@ -31,12 +31,19 @@ void main() {
     };
     
     conn.onClosed = (int status, String reason) {
-      print('closed with $status for $reason and ' + clients.length + ' clients remaining...');
-      clients.length--;
+      print('closed with $status for $reason');
+      var result = {};
+      result['type'] = 3;
+      result['message'] = 'Someone left the game.';
+      conn.send(JSON.stringify(result));
     };
           
     conn.onError = (e) {
-      print('Error was $e and ' + clients.length + ' clients remaining...');
+      print('Error was $e');
+      var result = {};
+      result['type'] = 4;
+      result['message'] = 'Error has occured.';
+      conn.send(JSON.stringify(result));
     };
   };
   print('WebSocket server is running...');
