9070391831ddf415f692c84de3f2ce3c17b00a39
stepiiik <stepan.zikmund@gmail.com>
2012-04-29 13:37:14 +0200

    First version of receiver

--- client/DartPong.dart
+++ client/DartPong.dart
@@ -11,7 +11,7 @@
 #source('HandlerListener.dart');
 #source('KillingZone.dart');
 #source('TeleportZone.dart');
-
+#source('MessageReceiver.dart');
 
 void main() {
   Element container =  document.query('#mainAppContainer'); // TODO: vypeknit
@@ -39,6 +39,12 @@ class DartPong {
     Handler rightHandler = new VerticalHandler( 'right_handler', 580, 300, 10, 100, 0, 30 );
     map.add(rightHandler);
     handlers.add(rightHandler);
+    Handler bottomHandler = new HorizontalHandler( 'bottom_handler', 250, 580, 100, 10, 0, 30 );
+    map.add(bottomHandler);
+    handlers.add(bottomHandler);
+    Handler topHandler = new HorizontalHandler( 'top_handler', 250, 10, 100, 10, 0, 30 );
+    map.add(topHandler);
+    handlers.add(topHandler);
     
     CollisionObject leftWall = new KillingZone( 'left_wall', 0, 0, 1, 600, 0, 0, leftHandler );
     map.add(leftWall);   
@@ -81,7 +87,15 @@ class DartPong {
     ticker.start();
   }
   
-  void runMultiPlayer() {
+  void runMultiPlayer()
+  {
+    WebSocket ws = new WebSocket("ws://localhost:3000");
+    MessageReceiver receiver = new MessageReceiver(ws);
+    
+    ws.send(data);
+  }
+  
+  void prepareGameForMultiplayer(num i) {
     Ball ball = new Ball( 'ball', 300, 300, 10, 10, 6, 1 );
     
     List<CollisionObject> map = new List<CollisionObject>();
@@ -94,38 +108,23 @@ class DartPong {
     Handler rightHandler = new VerticalHandler( 'right_handler', 580, 300, 10, 100, 0, 30 );
     map.add(rightHandler);
     handlers.add(rightHandler);
-    Handler bottomHandler = new HorizontalHandler( 'bottom_handler', 250, 580, 100, 10, 0, 30 );
-    map.add(bottomHandler);
-    handlers.add(bottomHandler);
-    Handler topHandler = new HorizontalHandler( 'top_handler', 250, 10, 100, 10, 0, 30 );
-    map.add(topHandler);
-    handlers.add(topHandler);
     
-    // CollisionObject leftWall = new KillingZone( 'left_wall', 0, 0, 1, 600, 0, 0, leftHandler );
-    // map.add(leftWall);
     CollisionObject leftWall = new TeleportZone( 'left_wall', 0, 0, 1, 600, 0, 0);
     map.add(leftWall);
        
-    CollisionObject topWall = new KillingZone( 'top_wall', 0, 0, 600, 1, 0, 0, topHandler );
+    CollisionObject topWall = new CollisionObject( 'top_wall', 0, 0, 600, 1, 0, 0 );
     map.add(topWall);
-    killingZones.add(topWall);
+    CollisionObject bottomWall = new CollisionObject( 'bottom_wall', 0, 600, 600, 1, 0, 0 );
+    map.add(bottomWall);
+    
     CollisionObject rightWall = new KillingZone( 'right_wall', 600, 0, 1, 600, 0, 0, rightHandler );
     map.add(rightWall);
     killingZones.add(rightWall);
-    CollisionObject bottomWall = new KillingZone( 'bottom_wall', 0, 600, 600, 1, 0, 0, bottomHandler );
-    map.add(bottomWall);
-    killingZones.add(bottomWall);
-    
-    CollisionObject test = new CollisionObject( 'test', 350, 350, 100, 100, 0, 0 );
-    map.add(test);
-    
     
     Arena arena = new Arena( map, ball );
     
     leftHandler.arena = arena;
     rightHandler.arena = arena;
-    bottomHandler.arena = arena;
-    topHandler.arena = arena;
     
     leftWall.arena = arena;
     rightWall.arena = arena;
@@ -137,8 +136,6 @@ class DartPong {
     
     HandlerListener leftHandlerListener  = new HandlerListener(leftHandler, 38, 40, renderer);
     HandlerListener rightHandlerListener = new HandlerListener(rightHandler, 83, 88, renderer);
-    HandlerListener bottomHandlerListener = new HandlerListener(bottomHandler, 66, 78, renderer);
-    HandlerListener topHandlerListener = new HandlerListener(topHandler, 85, 73, renderer);
     
     Ticker ticker = new Ticker( 10, arena, renderer, window );
     ticker.start();
