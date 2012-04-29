4307bffd6099bef830520d5d985f01cff977e800
stepiiik <stepan.zikmund@gmail.com>
2012-04-29 10:31:24 +0200

    Removed logging and comments

--- client/Ball.dart
+++ client/Ball.dart
@@ -10,42 +10,10 @@ class Ball extends CollisionObject {
   }
   
   void onCollisionWith(CollisionObject o) {
-    
-    // bounce
-    print('Collision BALL');
-
     if (this.x > o.x && this.x+this.width < o.x+o.width) {
       this.direction = flipAngleHorizontal(this.direction);
     } else {
       this.direction = flipAngleVertical(this.direction);
-    }
-    
-    
-    /*
-    // top
-    if (this.y < o.y+o.height && this.y+this.height > o.y+o.height && this.x+this.width > o.x && o.x+o.width > this.x) {
-      this.direction = 2*Math.PI - this.direction;
-    } 
-
-    // bottom
-    if (this.y+this.height > o.y && this.y+this.height < o.y+o.height && this.x+this.width > o.x && o.x+o.width > this.x.ceil()) {
-      this.direction = 2*Math.PI - this.direction;
-    } 
-    
-    // left
-    if (this.x < o.x+o.width && this.x>o.x && this.y+this.height > o.y && this.y < o.y+o.height) {
-      this.direction = Math.PI - this.direction;
-    } 
-
-    // right
-    if (this.x+this.width > o.x && this.x+this.width<o.x+o.width && this.y+this.height > o.y && this.y < o.y+o.height) {
-      this.direction = Math.PI - this.direction; 
-    } 
-
-    */
-    
+    }  
   }
-  
-
-
 }
