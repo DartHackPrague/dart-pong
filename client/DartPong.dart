5b0a570afb985d753f0c99aae0ae5c3bccf5ae98
Petr Pechacek <petr.pechacek@gmail.com>
2012-04-29 11:37:11 +0200

    Resize map

--- client/DartPong.dart
+++ client/DartPong.dart
@@ -36,26 +36,26 @@ class DartPong {
     Handler leftHandler  = new VerticalHandler( 'left_handler', 10, 300, 10, 100, 0, 30 );
     map.add(leftHandler);
     handlers.add(leftHandler);
-    Handler rightHandler = new VerticalHandler( 'right_handler', 780, 300, 10, 100, 0, 30 );
+    Handler rightHandler = new VerticalHandler( 'right_handler', 580, 300, 10, 100, 0, 30 );
     map.add(rightHandler);
     handlers.add(rightHandler);
-    Handler bottomHandler = new HorizontalHandler( 'bottom_handler', 350, 580, 100, 10, 0, 30 );
+    Handler bottomHandler = new HorizontalHandler( 'bottom_handler', 250, 580, 100, 10, 0, 30 );
     map.add(bottomHandler);
     handlers.add(bottomHandler);
-    Handler topHandler = new HorizontalHandler( 'top_handler', 350, 10, 100, 10, 0, 30 );
+    Handler topHandler = new HorizontalHandler( 'top_handler', 250, 10, 100, 10, 0, 30 );
     map.add(topHandler);
     handlers.add(topHandler);
     
     CollisionObject leftWall = new KillingZone( 'left_wall', 0, 0, 1, 600, 0, 0, leftHandler );
     map.add(leftWall);   
     killingZones.add(leftWall);   
-    CollisionObject topWall = new KillingZone( 'top_wall', 0, 0, 800, 1, 0, 0, topHandler );
+    CollisionObject topWall = new KillingZone( 'top_wall', 0, 0, 600, 1, 0, 0, topHandler );
     map.add(topWall);
     killingZones.add(topWall);
-    CollisionObject rightWall = new KillingZone( 'right_wall', 800, 0, 1, 600, 0, 0, rightHandler );
+    CollisionObject rightWall = new KillingZone( 'right_wall', 600, 0, 1, 600, 0, 0, rightHandler );
     map.add(rightWall);
     killingZones.add(rightWall);
-    CollisionObject bottomWall = new KillingZone( 'bottom_wall', 0, 600, 800, 1, 0, 0, bottomHandler );
+    CollisionObject bottomWall = new KillingZone( 'bottom_wall', 0, 600, 600, 1, 0, 0, bottomHandler );
     map.add(bottomWall);
     killingZones.add(bottomWall);
     
